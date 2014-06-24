e3b178a12c157a90354a336db01767f257cb6e19
slowsushi <wuytsky@gmail.com>
2014-06-24 14:40:02 +0200

    now it shouold work

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ $(document).ready(setInterval(function() {
     
     var colors = ["#CCCCCC", "#333333", "#990099", "#71C671", "#F4A460", "#FFFF00", "#00FF00", "#104E8B", "#8B4789", "#B0171F"];
     var rand = Math.floor(Math.random() * colors.length);
-    $('.logo').css("background-color", colors[rand]);
+    $('#logo').css("background-color", colors[rand]);
     
 }, 1000));
