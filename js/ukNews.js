a8026539e98bf431c807e125e944cfc74a5e5111
slowsushi <wuytsky@gmail.com>
2014-06-26 16:09:05 +0200

    trying api ukNews

--- js/ukNews.js
+++ js/ukNews.js
@@ -1,7 +1,13 @@
-$(document).ready( function() {
- $.getJSON("json_data.json", function(data){
-       $.each(data.response, function(){
-	         $("ul").append("<li> "+this['results']+"</li>
-                                <br />");
-	   });
- });
+     $.ajax({
+
+      url: "http://content.guardianapis.com/search?q=uk+news&show-fields=trailText",
+
+      dataType: "jsonp",
+
+      success: function(data) {
+        $.each(data.response.results, function () {
+                 $( "p ul").append('<li> this['webTitle'] +'</a><p>' + this['fields'].trailText + '</p></li>');
+        });
+
+      }});
+  });
